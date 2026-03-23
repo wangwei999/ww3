17857a109d1462577def143b1486b20174b0c10b
用户4267383201836 <697558349121803-4267383201836@noreply.coze.cn>
2026-03-23 15:55:29 +0800

    feat: 久坐提醒弹出间隔增加1分钟选项

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -937,7 +937,7 @@ export default function Home() {
                     <SelectValue />
                   </SelectTrigger>
                   <SelectContent>
-                    {[5, 10, 15, 20, 25, 30, 45, 60].map(min => (
+                    {[1, 5, 10, 15, 20, 25, 30, 45, 60].map(min => (
                       <SelectItem key={min} value={min.toString()}>
                         {min} 分钟
                       </SelectItem>
